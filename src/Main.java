3a38a5419cf502d55909bd6ba64f83c3e06d0e19
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2024-11-26 17:01:59 +0100

    Modificacion de metodos ya existentes , modelando pagina para un mejor entendimiento

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
 
@@ -14,6 +12,7 @@ public class Main {
         JFrame ventana1 = new JFrame("Ventana 1");
         ventana1.setSize(1500, 800);
         ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana1.setBackground(Color.WHITE);
         ventana1.setResizable(false);
         ventana1.setLayout(null);
 
@@ -21,36 +20,33 @@ public class Main {
 
         JLabel header = new JLabel("Header");
         header.setFont(new Font("Arial", Font.BOLD, 20));
-        header.setBackground(Color.WHITE);
         header.setForeground(Color.BLACK);
         header.setBounds(0, 0, 1500, 50);
         header.setOpaque(true);
 
         // ImagenIcon icon add
 
-        ImageIcon icon = new ImageIcon("Images/logo_header.png");
-        icon.setImage(icon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
-
-
-        header.setIcon(icon);
+        ImageIcon seticon = resizeimage(new ImageIcon("Images/logo_header.png"), 0.5);
+        JLabel icon = new JLabel(seticon);
+        header.add(icon);
 
         // Add Objects
         ventana1.add(header);
+
         ventana1.setVisible(true);
 
     }
 
     // Method to resize the image
-    public static ImageIcon redimensionarImagen(String rutaImagen, double porcentaje) {
-        ImageIcon imagenOriginal = new ImageIcon(rutaImagen);
+    public static ImageIcon resizeimage(ImageIcon icon, double porcentaje) {
 
-        int anchoOriginal = imagenOriginal.getIconWidth();
-        int altoOriginal = imagenOriginal.getIconHeight();
+        int anchoOriginal = icon.getIconWidth();
+        int altoOriginal = icon.getIconHeight();
 
         int nuevoAncho = (int) (anchoOriginal * porcentaje);
         int nuevoAlto = (int) (altoOriginal * porcentaje);
 
-        Image imagenRedimensionada = imagenOriginal.getImage().getScaledInstance(
+        Image imagenRedimensionada = icon.getImage().getScaledInstance(
                 nuevoAncho, nuevoAlto, Image.SCALE_SMOOTH
         );
 
@@ -58,38 +54,49 @@ public class Main {
     }
 
     // Method to position the images
-    public static void posicionarImagen(JLabel imagen, int x, int y) {
+    public static void positionLabel(JLabel imagen, int x, int y) {
         imagen.setBounds(x, y, imagen.getIcon().getIconWidth(), imagen.getIcon().getIconHeight());
     }
+   
 
-    public static void agregarEfectoHover(JLabel label, String rutaImagen, double porcentajeOriginal, double porcentajeHover) {
+    // Method to add hover effect with ImageIcon
+    public static void addEfectHover(JLabel label, ImageIcon originalIcon, double originalPercentage, double hoverPercentage) {
+        // Resize the image in advance to avoid recalculating it during each event
+        ImageIcon resizedHoverIcon = resizeimage(originalIcon, hoverPercentage);
+        ImageIcon resizedOriginalIcon = resizeimage(originalIcon, originalPercentage);
+
         label.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseEntered(java.awt.event.MouseEvent e) {
-                label.setIcon(redimensionarImagen(rutaImagen, porcentajeHover)); // Aumentar tamaño al hover
+                label.setIcon(resizedHoverIcon); // Change to hover size
             }
 
             @Override
             public void mouseExited(java.awt.event.MouseEvent e) {
-                label.setIcon(redimensionarImagen(rutaImagen, porcentajeOriginal)); // Volver al tamaño original
+                label.setIcon(resizedOriginalIcon); // Revert to original size
             }
         });
     }
 
-    // Método para cambiar el tamaño al hacer clic
-    static void agregarEfectoClic(JLabel label, String rutaImagen, double porcentajeOriginal, double porcentajeClic) {
+    // Method to add click effect with ImageIcon
+    public static void addEfectClick(JLabel label, ImageIcon originalIcon, double originalPercentage, double clickPercentage) {
+        // Resize the image in advance to avoid recalculating it during each event
+        ImageIcon resizedClickIcon = resizeimage(originalIcon, clickPercentage);
+        ImageIcon resizedOriginalIcon = resizeimage(originalIcon, originalPercentage);
+
         label.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mousePressed(java.awt.event.MouseEvent e) {
-                label.setIcon(redimensionarImagen(rutaImagen, porcentajeClic)); // Aumentar tamaño al hacer clic
+                label.setIcon(resizedClickIcon); // Change to click size
             }
 
             @Override
             public void mouseReleased(java.awt.event.MouseEvent e) {
-                label.setIcon(redimensionarImagen(rutaImagen, porcentajeOriginal)); // Volver al tamaño original
+                label.setIcon(resizedOriginalIcon); // Revert to original size
             }
         });
     }
+
 
 
     public static void main(String[] args) {
