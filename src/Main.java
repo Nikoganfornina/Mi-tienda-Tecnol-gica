e652c7945525962d8bd052304ad4b9534e26fa12
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2024-11-26 16:01:58 +0100

    Comiteo de  guardado del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        JFrame ventana = new JFrame("Tienda Tecnologica");
+
+        ventana.setLayout(null);
+
+
+        ventana.setSize(1500, 1000);
+        ventana.setResizable(false);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // ventana.setUndecorated(true);
+        ventana.setVisible(true);
+
+        JButton boton = new JButton();
+
+
+    }
+    public static ImageIcon redimensionarImagen(String rutaImagen, double porcentaje) {
+        ImageIcon imagenOriginal = new ImageIcon(rutaImagen);
+
+        int anchoOriginal = imagenOriginal.getIconWidth();
+        int altoOriginal = imagenOriginal.getIconHeight();
+
+        int nuevoAncho = (int) (anchoOriginal * porcentaje);
+        int nuevoAlto = (int) (altoOriginal * porcentaje);
+
+        Image imagenRedimensionada = imagenOriginal.getImage().getScaledInstance(
+                nuevoAncho, nuevoAlto, Image.SCALE_SMOOTH
+        );
+
+        return new ImageIcon(imagenRedimensionada);
+    }
+
+    // Method to position the images
+    public static void posicionarImagen(JLabel imagen, int x, int y) {
+        imagen.setBounds(x, y, imagen.getIcon().getIconWidth(), imagen.getIcon().getIconHeight());
+    }
+    public static void agregarEfectoHover(JLabel label, String rutaImagen, double porcentajeOriginal, double porcentajeHover) {
+        label.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                label.setIcon(redimensionarImagen(rutaImagen, porcentajeHover)); // Aumentar tamaño al hover
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                label.setIcon(redimensionarImagen(rutaImagen, porcentajeOriginal)); // Volver al tamaño original
+            }
+        });
+    }
+
+    // Método para cambiar el tamaño al hacer clic
+    static void agregarEfectoClic(JLabel label, String rutaImagen, double porcentajeOriginal, double porcentajeClic) {
+        label.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent e) {
+                label.setIcon(redimensionarImagen(rutaImagen, porcentajeClic)); // Aumentar tamaño al hacer clic
+            }
+
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent e) {
+                label.setIcon(redimensionarImagen(rutaImagen, porcentajeOriginal)); // Volver al tamaño original
+            }
+        });
     }
 }
