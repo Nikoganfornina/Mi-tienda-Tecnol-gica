e7f19c01fbd22a691a1666d1eb3bf71564866267
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2024-11-27 17:40:29 +0100

    Añadida clase producto para mejor interacion con la bbdd

--- src/ColorsNk.java
+++ src/ColorsNk.java
@@ -8,6 +8,10 @@ public class ColorsNk implements Paint, java.io.Serializable {
     public static final Color Verde_Nk     = new Color(99, 128, 111);
 
     public static final Color Transparent_Nk = new Color(0, 0, 0, 0);
+
+    public static final Color verde_claro_Nk = new Color(99, 128, 111);
+
+
     @Override
     public PaintContext createContext(ColorModel cm, Rectangle deviceBounds, Rectangle2D userBounds, AffineTransform xform, RenderingHints hints) {
         return null;
