eb3de636c6cce58d59e60b91adf7a77f3b08a054
Nicolás Ganfornina García <nikoganfornina@gmail.com>
2024-11-27 17:55:36 +0100

    Añadido al proyecto el archivo .json

--- src/Producto.java
+++ src/Producto.java
@@ -1,2 +1,71 @@
+import java.util.ArrayList;
+
 public class Producto {
+
+    private int id;
+    String nombre;
+    double precio;
+    private String descripcion;
+    private String foto; // Ruta o URL de la foto del producto
+    private ArrayList<String> caracteristicas; // Lista de características
+
+    // Constructor
+    public Producto(int id, String nombre, double precio, String descripcion, String foto, ArrayList<String> caracteristicas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.foto = foto;
+        this.caracteristicas = caracteristicas;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public ArrayList<String> getCaracteristicas() {
+        return caracteristicas;
+    }
+
+    public void setCaracteristicas(ArrayList<String> caracteristicas) {
+        this.caracteristicas = caracteristicas;
+    }
 }
+
